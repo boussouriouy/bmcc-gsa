469a192334c6c759c558d5de5933883bda75540a
Mamadou Boussouriou Diallo <43155860+boussouriouy@users.noreply.github.com>
2020-06-20 18:18:20 -0400

    Local settings

--- gsabmcc/settings.py
+++ gsabmcc/settings.py
@@ -130,3 +130,8 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Go on")
